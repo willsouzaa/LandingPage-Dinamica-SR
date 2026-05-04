c76b2cdaf281f627785e5245d37e7df4bf29935a
willsouzaa <pauloamancio79@gmail.com>
2026-05-04 09:35:49 -0300

    Foi feito o redirecionamento do dominio base para lancamentos.sanremoimoveis.com.br

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
   },
+  
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://lancamentos.sanremoimoveis.com.br',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
